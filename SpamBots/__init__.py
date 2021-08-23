ba723dd2a1250b29f10d4bb7253b75093fee00a3
ANMOL12334 <61105838+ANMOL12334@users.noreply.github.com>
2021-08-23 17:19:57 +0530

    Update __init__.py

--- SpamBots/__init__.py
+++ SpamBots/__init__.py
@@ -22,6 +22,16 @@ BOT_TOKEN7 = config("BOT_TOKEN7", default=None)
 BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
+BOT_TOKEN11 = config("BOT_TOKEN11", default=None)
+BOT_TOKEN12 = config("BOT_TOKEN12", default=None)
+BOT_TOKEN13 = config("BOT_TOKEN13", default=None)
+BOT_TOKEN14 = config("BOT_TOKEN14", default=None)
+BOT_TOKEN15 = config("BOT_TOKEN15", default=None)
+BOT_TOKEN16 = config("BOT_TOKEN16", default=None)
+BOT_TOKEN17 = config("BOT_TOKEN17", default=None)
+BOT_TOKEN18 = config("BOT_TOKEN18", default=None)
+BOT_TOKEN19 = config("BOT_TOKEN19", default=None)
+BOT_TOKEN20 = config("BOT_TOKEN20", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
 UstaD = TelegramClient('UstaD', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
@@ -42,4 +52,24 @@ UstaD8 = TelegramClient('UstaD8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8)
 
 UstaD9 = TelegramClient('UstaD9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
 
-UstaD10 = TelegramClient('UstaD10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10) 
+UstaD10 = TelegramClient('UstaD10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+
+UstaD11 = TelegramClient('UstaD11', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+
+UstaD12 = TelegramClient('UstaD12', APP_ID, API_HASH).start(bot_token=BOT_TOKEN2) 
+
+UstaD13 = TelegramClient('UstaD13', APP_ID, API_HASH).start(bot_token=BOT_TOKEN3) 
+
+UstaD14 = TelegramClient('UstaD14', APP_ID, API_HASH).start(bot_token=BOT_TOKEN4) 
+
+UstaD15 = TelegramClient('UstaD15', APP_ID, API_HASH).start(bot_token=BOT_TOKEN5) 
+
+UstaD16 = TelegramClient('UstaD16', APP_ID, API_HASH).start(bot_token=BOT_TOKEN6) 
+
+UstaD17 = TelegramClient('UstaD17', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7) 
+
+UstaD18 = TelegramClient('UstaD18', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
+
+UstaD19 = TelegramClient('UstaD19', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
+
+UstaD20 = TelegramClient('UstaD20', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10) 
