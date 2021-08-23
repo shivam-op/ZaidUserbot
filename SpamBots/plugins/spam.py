fd15f3d9e2a1b3d92f7b899048020b3c5e2cbfee
ANMOL12334 <61105838+ANMOL12334@users.noreply.github.com>
2021-08-23 18:00:48 +0530

    Update spam.py

--- SpamBots/plugins/spam.py
+++ SpamBots/plugins/spam.py
@@ -86,6 +86,9 @@ for x in SUDO_USERS:
 )
 @UstaD20.on(
     events.NewMessage(pattern="^/spam", func=lambda e: e.sender_id in SMEX_USERS)
+)
+
+
 
 async def spam(e):
     if e.sender_id in SMEX_USERS:
