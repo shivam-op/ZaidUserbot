8eee3658d90c70692f6d44a04cdc830c8111a549
ANMOL12334 <61105838+ANMOL12334@users.noreply.github.com>
2021-08-23 17:25:25 +0530

    Update bigspam.py

--- SpamBots/plugins/bigspam.py
+++ SpamBots/plugins/bigspam.py
@@ -19,7 +19,7 @@ import os
 from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from .. import UstaD, UstaD2, UstaD3, UstaD4, UstaD5, UstaD6, UstaD7, UstaD8, UstaD9, UstaD10, SUDO_USERS
+from .. import UstaD, UstaD2, UstaD3, UstaD4, UstaD5, UstaD6, UstaD7, UstaD8, UstaD9, UstaD10, UstaD11, UstaD12, UstaD13, UstaD15 , UstaD16, UstaD17, UstaD18, UstaD19, UstaD20, SUDO_USERS
 
 SMEX_USERS = []
 for x in SUDO_USERS:
