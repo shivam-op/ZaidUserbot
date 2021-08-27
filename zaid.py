c736df540237938cb3c0da17d6f2677117fd892e
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-08-27 15:46:47 +0530

    Create zaid.py

---
+++ zaid.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/Itsunknown-12/TgBot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
